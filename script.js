6a59c75eee0493c1edde24a37c8a91a14b101a42
shubhamkash <76610307+shubhamkash@users.noreply.github.com>
2023-04-13 23:47:14 +0530

    VS 2 - Updated color generation logic from random to fixed, added reply button, other fixes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const bricks = document.querySelectorAll('.brick');
+const gameArea = document.querySelector("#column-view");
+const replayButton = document.querySelector('.button');
 const targetColor = document.querySelector('#target-color');
 const score = document.querySelector('#score-fist-char');
 const chance = document.querySelector('#chance-fist-char');
@@ -8,38 +10,68 @@ const gameOver = document.querySelector('#game-over');
 let currentScore = 0;
 let currentChance = 3;
 let targetIndex;
+let randomColorIndex = 0;
+let usedColorInex =  [];
+let usedColor =  [];
+let colorName;
 let plusSign ="+";
 let minusSign = "-";
+let realIndex;
 const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'gray'];
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)];
+
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * letters.length)];
+//   }
+//   return color;
+// }
+
+function updateBoxColors() {
+  randomColorIndex = Math.floor(Math.random() * colors.length)
+  if (!usedColorInex.includes(randomColorIndex)){
+    usedColorInex.push(randomColorIndex);
+    usedColor.push(colors[randomColorIndex]);
+    colorName = colors[randomColorIndex]
   }
-  return color;
+  else{
+    if (usedColorInex.length < colors.length)
+    {
+      updateBoxColors();
+    }
+  }
+  return colorName;
 }
+
 
 function updateTargetColor() {
   targetIndex = Math.floor(Math.random() * colors.length);
-  targetColor.textContent = colors[targetIndex];
+  // targetColor.textContent = colors[targetIndex];
 }
 
 function updatebrickColors() {
+  usedColorInex = [];
+  usedColor = []; 
   bricks.forEach(brick => {
-    brick.style.backgroundColor = getRandomColor();
+ 
+    brick.style.backgroundColor = updateBoxColors();
     brick.removeEventListener('click', handleClick);
     brick.addEventListener('click', handleClick);
   });
-  const targetbrick = bricks[targetIndex];
-  targetbrick.style.backgroundColor = colors[targetIndex];
-  targetColor.style.color = colors[targetIndex];
+
+  targetColor.style.color = usedColor[targetIndex];
+  targetColor.textContent = usedColor[targetIndex];
+  realIndex = usedColor.indexOf(usedColor[targetIndex]);
+  const targetbrick = bricks[realIndex];
+  // targetbrick.style.backgroundColor = colors[targetIndex];
+
   targetbrick.addEventListener('click', handleClick);
 }
 
 function handleClick(event) {
-   if (event.target === bricks[targetIndex]) {
+   if (event.target === bricks[realIndex]) {
     currentScore++;
     chancePlusMinus.textContent = plusSign;
     chancePlusMinus.style.color = "green";
@@ -59,6 +91,8 @@ function handleClick(event) {
    chance.textContent = currentChance;
    if(currentChance === 0){
         instruction.style.display = "none";
+        gameArea.style.display = "none";
+        replayButton.style.display = "block";
         gameOver.style.display = "block"; 
    }
   updateTargetColor();
